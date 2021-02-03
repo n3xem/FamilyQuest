b83e7d5fbcc32660beb5ec9c38e4f5cc546ea00a
Yukyan <yukyanjpn@gmail.com>
2021-02-03 12:19:31 +0900

    Add(settings): INSTALLED_APPS + TaskConfig

--- familyquest/settings.py
+++ familyquest/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'task.apps.TaskConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
