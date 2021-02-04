dd0d802ca0a824dcb9a5b0318781b4222435f8aa
Yukyan <yukyanjpn@gmail.com>
2021-02-04 14:36:45 +0900

    Add(shop): settings.pyにアプリを追記

--- familyquest/settings.py
+++ familyquest/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'task.apps.TaskConfig',
+    'shop.apps.ShopConfig',
     # django-allauth
     'allauth',
     'allauth.account',
