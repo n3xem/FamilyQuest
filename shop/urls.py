bf6fe7305e728fe7198e7e3e8c078a8dc400f5c8
Yukyan <yukyanjpn@gmail.com>
2021-02-16 11:50:55 +0900

    Fix: 購入時経験値が足りない場合に遷移

--- shop/urls.py
+++ shop/urls.py
@@ -6,4 +6,5 @@ app_name = 'shop'
 urlpatterns = [
   path('', views.IndexView.as_view(), name='index'),
   path('taskbg/<int:pk>/buy', views.task_background_item_buy, name='taskbg_buy'),
+  path('taskbg/<int:pk>/buy-fail', views.task_background_buy_fail, name='taskbg_buy_fail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from shop.models import TaskBackgroundItem, UsersTaskBackgroundItems
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
+
 
 class IndexView(generic.ListView):
     model =  TaskBackgroundItem
@@ -9,11 +10,24 @@ class IndexView(generic.ListView):
 def task_background_item_buy(request, pk):
     task_background_item = TaskBackgroundItem.objects.get(pk=pk)
 
+    if request.user.experience_point - task_background_item.experience_point_cost < 0:
+        return redirect('shop:taskbg_buy_fail', pk=pk)
+    
     request.user.experience_point -= task_background_item.experience_point_cost
     request.user.save()
     new_users_task_background_items = UsersTaskBackgroundItems(user=request.user, item=task_background_item)
     new_users_task_background_items.save()
 
-    context = { 'item': task_background_item }
-
+    context = { 
+        'item': task_background_item 
+    }
     return render(request, 'shop/buy.html', context)
+
+def task_background_buy_fail(request, pk):
+    task_background_item = TaskBackgroundItem.objects.get(pk=pk)
+
+    context = { 
+        'item': task_background_item,
+        'diff': request.user.experience_point - task_background_item.experience_point_cost,
+    }
+    return render(request, 'shop/buy-fail.html', context)
